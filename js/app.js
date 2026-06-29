bd229bbb9385d3423a5043f45d74b546d2df2eed
Sayed Ali <salimustafa2005@gmail.com>
2026-06-29 16:00:14 +0300

    Added gameOver condition when tries are over

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
-let word = "BREAK";
+let word = 'SLIDE'
 let currentWord = 0;
 let currentLetter = 0;
 let gameOver = false;
 const wordLength = 5;
 const numOfTries = 6;
 let guess = '';
+const ans = document.querySelector('#answer')
 
 function addLetter(e) {
-
     console.log(`${currentWord}-${currentLetter}`)
     let currentTile = document.getElementById(`${currentWord}-${currentLetter}`)
     let w = e.key.toUpperCase();
-
     if (currentLetter == 5 && e.key == 'Enter') {
+        checkWord()
+        gameOverF()
         if (guess == word) {
             gameOver = true
+            // ans.innerHTML = word
             console.log("You Won!")
         }
         else {
@@ -33,19 +35,39 @@ function addLetter(e) {
         return
     }
     if (e.code.startsWith('Key') && currentLetter < 5) {
-        if (w >= 'A' && w <= 'Z'){
-        console.log(currentTile)
-        currentTile.innerHTML = w
-        currentLetter++
-        guess = guess + w
-        // guess.push(w)
-        console.log(guess)
-        return}
+        if (w >= 'A' && w <= 'Z') {
+            console.log(currentTile)
+            currentTile.innerHTML = w
+            currentLetter++
+            guess = guess + w
+            // guess.push(w)
+            console.log(guess)
+            return
+        }
     }
 }
 
-// function win(){
+function checkWord() {
+    for (let i = 0; i < wordLength; i++) {
+        let currentTile = document.getElementById(`${currentWord}-${i}`)
+        let guessedLetter = guess[i];
+        if (guessedLetter === word[i]) {
+            currentTile.classList.add("correct")
+        } else if (word.includes(guessedLetter)) {
+            currentTile.classList.add("includes")
+        } else {
+            currentTile.classList.add("absent")
+        }
 
-// }
+    }
+}
+
+function gameOverF() {
+    console.log('currentWord', currentWord)
+    if (currentWord === 5) {
+        gameOver = true;
+        ans.innerHTML = word;
+    }
+}
 
 addEventListener('keyup', addLetter);
