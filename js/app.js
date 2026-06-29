b9e0bab7d0b4b9175716edc6f460881baf9db95d
Sayed Ali <salimustafa2005@gmail.com>
2026-06-29 14:32:56 +0300

    fixed the to edit the variable too not only the innerHTML and input accepts letters only

--- js/app.js
+++ js/app.js
@@ -4,29 +4,48 @@ let currentLetter = 0;
 let gameOver = false;
 const wordLength = 5;
 const numOfTries = 6;
+let guess = '';
 
 function addLetter(e) {
+
     console.log(`${currentWord}-${currentLetter}`)
     let currentTile = document.getElementById(`${currentWord}-${currentLetter}`)
+    let w = e.key.toUpperCase();
 
     if (currentLetter == 5 && e.key == 'Enter') {
-        console.log("IN IF")
-        currentWord++;
-        currentLetter = 0;
-        return
+        if (guess == word) {
+            gameOver = true
+            console.log("You Won!")
+        }
+        else {
+            console.log("IN IF")
+            currentWord++;
+            currentLetter = 0;
+            guess = '';
+            return
+        }
     }
-    if (e.key === 'Backspace'){
+    if (currentLetter > 0 && e.key === 'Backspace') {
         currentLetter--
         let currentTile = document.getElementById(`${currentWord}-${currentLetter}`)
         currentTile.innerHTML = ''
+        guess = guess.slice(0, -1);
         return
     }
-    if (currentLetter<5){
-    console.log(currentTile)
-    currentTile.innerHTML = e.key
-    currentLetter++
-    return
+    if (e.code.startsWith('Key') && currentLetter < 5) {
+        if (w >= 'A' && w <= 'Z'){
+        console.log(currentTile)
+        currentTile.innerHTML = w
+        currentLetter++
+        guess = guess + w
+        // guess.push(w)
+        console.log(guess)
+        return}
     }
 }
+
+// function win(){
+
+// }
 
 addEventListener('keyup', addLetter);
