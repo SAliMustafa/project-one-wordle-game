4c4c073cc07b9600a748e602ef3b40e2e348051d
Sayed Ali <salimustafa2005@gmail.com>
2026-06-29 10:44:10 +0300

    Backspace works

--- js/app.js
+++ js/app.js
@@ -6,25 +6,27 @@ const wordLength = 5;
 const numOfTries = 6;
 
 function addLetter(e) {
-    // for (let w = 0; w <= wordLength; w++){
-    //     for(let t = 0; t <= numOfTries; t++){
-    // const letter = document.querySelector(".tile")
-    // letter.innerHTML = e.key
-
     console.log(`${currentWord}-${currentLetter}`)
     let currentTile = document.getElementById(`${currentWord}-${currentLetter}`)
+
     if (currentLetter == 5 && e.key == 'Enter') {
         console.log("IN IF")
         currentWord++;
         currentLetter = 0;
         return
     }
+    if (e.key === 'Backspace'){
+        currentLetter--
+        let currentTile = document.getElementById(`${currentWord}-${currentLetter}`)
+        currentTile.innerHTML = ''
+        return
+    }
+    if (currentLetter<5){
     console.log(currentTile)
     currentTile.innerHTML = e.key
     currentLetter++
-
-    //         }
-    //         currentWord++
-    //     }
+    return
+    }
 }
+
 addEventListener('keyup', addLetter);
