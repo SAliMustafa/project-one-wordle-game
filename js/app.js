629c7e1063763a8148c848b74d766bd1f1dfaa10
Sayed Ali <salimustafa2005@gmail.com>
2026-06-29 21:02:32 +0300

    made the word random and added game over keyboard lock

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
-let word = 'SLIDE'
+const words = ['SLIDE', 'BREAK', 'SAYED', 'FAITH', 'AGREE', 'AMUSE', 'ANGLE', 'FAVOR', 'GRACE', 'HEART', 'LEARN', 'MERCY', 'POWER', 'PRIME', 'PEACE', 'VALUE']
 let currentWord = 0;
 let currentLetter = 0;
 let gameOver = false;
 const wordLength = 5;
 const numOfTries = 6;
 let guess = '';
+const correctWord = words[Math.floor(Math.random() * words.length)] 
 const ans = document.querySelector('#answer')
 
 function addLetter(e) {
+    if (gameOver){return}
     console.log(`${currentWord}-${currentLetter}`)
     let currentTile = document.getElementById(`${currentWord}-${currentLetter}`)
     let w = e.key.toUpperCase();
     if (currentLetter == 5 && e.key == 'Enter') {
         checkWord()
         gameOverF()
-        if (guess == word) {
+        if (guess == correctWord) {
             gameOver = true
             // ans.innerHTML = word
             console.log("You Won!")
@@ -51,9 +53,9 @@ function checkWord() {
     for (let i = 0; i < wordLength; i++) {
         let currentTile = document.getElementById(`${currentWord}-${i}`)
         let guessedLetter = guess[i];
-        if (guessedLetter === word[i]) {
+        if (guessedLetter === correctWord[i]) {
             currentTile.classList.add("correct")
-        } else if (word.includes(guessedLetter)) {
+        } else if (correctWord.includes(guessedLetter)) {
             currentTile.classList.add("includes")
         } else {
             currentTile.classList.add("absent")
@@ -66,8 +68,7 @@ function gameOverF() {
     console.log('currentWord', currentWord)
     if (currentWord === 5) {
         gameOver = true;
-        ans.innerHTML = word;
+        ans.innerHTML = correctWord;
     }
 }
-
 addEventListener('keyup', addLetter);
